1fd8131443c7458057e826caeb896531fb3618ee
Barbara Bogaczyk <bbogaczyk@gmail.com>
2021-05-12 23:59:59 +0800

    commit 3 files

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTime = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        
+        timer.invalidate() //start the timer again
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        totalTime = eggTime[hardness]!
+        progressBar.progress = 0.0 //reset the progress bar when press different button
+        secondsPassed = 0 //reset the timer to start from the begining
+        titleLabel.text = hardness // back title to original when the timer stops
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        //example functionality
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!" //change the tile when the timer ends
+            
+            let url = Bundle.main.url(forResource: "411", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+    }
+    
 }
